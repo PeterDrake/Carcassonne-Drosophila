287770638864f3e493e95094e7fe5acb4b38af32
quixoticdicker <cdicker@lclark.edu>
2014-01-30 15:18:12 -0800

    players can now enter names

--- src/Carcassonne.java
+++ src/Carcassonne.java
@@ -1,7 +1,15 @@
 public class Carcassonne {
 
+	private Player player1;
+	
 	public static void main(String[] args) {
 		System.out.println("Time flies like an arrow, fruit flies like a banana.");
+		
+		new Carcassonne();
+	}
+	
+	public Carcassonne() {
+		player1 = new Player();
 	}
 
 }
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,20 @@
-
 public class Player {
+	
+	private String name;
 
+	public Player() {
+		name = "";
+	}
+	
+	public Player(String newName) {
+		name = newName;
+	}
+
+	public void setName(String newName) {
+		name = newName;
+	}
+
+	public String getName() {
+		return name;
+	}
 }
