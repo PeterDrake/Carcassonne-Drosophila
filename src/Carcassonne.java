bdda456caa4821cc2fdd6b026ce85e8130acd4bb
quixoticdicker <cdicker@lclark.edu>
2014-01-30 15:35:47 -0800

    Now prompts for 1 name and welcomes the player

--- src/Carcassonne.java
+++ src/Carcassonne.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Carcassonne {
 
 	private Player player1;
+	private Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		System.out.println("Time flies like an arrow, fruit flies like a banana.");
@@ -10,6 +13,12 @@ public class Carcassonne {
 	
 	public Carcassonne() {
 		player1 = new Player();
+		
+		System.out.println("Please enter the name of player 1:");
+		player1.setName(sc.nextLine());
+		
+		System.out.println("Only one player right now? Remember, games are better with friends!");
+		System.out.println("Oh well, have fun " + player1.getName());
 	}
 
 }
