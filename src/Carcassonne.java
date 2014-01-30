dba0947465f96148271b2cc31ca085c565aba219
Sam Levenick <slevenick@lclark.edu>
2014-01-30 15:34:53 -0800

    Added a text file with the tiles. Started adding a scanner

--- src/Carcassonne.java
+++ src/Carcassonne.java
@@ -1,7 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Carcassonne {
+
+	
 
 	public static void main(String[] args) {
 		System.out.println("Time flies like an arrow, fruit flies like a banana.");
+	}
+	private Scanner myScanner;
+	
+	public void Carcassonne(){
+		try {
+			myScanner = new Scanner(new File("fileDist.txt"));
+		} catch (FileNotFoundException e) {
+			System.out.println("The filepath of the initial tiles is invalid");
+			e.printStackTrace();
+		}
+		while (myScanner.hasNextInt()) {
+	          int north = myScanner.nextInt();
+	      }
 	}
 
 }
