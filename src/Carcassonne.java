1819b0b4a8e7b89efc689981a51cc7a874fc847c
Sam Levenick <slevenick@lclark.edu>
2014-02-04 13:38:12 -0800

    Added some comments, a new method for setting up the tiles, and a Carcassonne test

--- src/Carcassonne.java
+++ src/Carcassonne.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Carcassonne {
 
 	private Tile[] tiles;
+	private Tile[] tempTiles;
 	public static final int TILES_IN_GAME = 71;
 	private Scanner myScanner;
 
@@ -15,13 +16,21 @@ public class Carcassonne {
 	
 	
 	public Carcassonne(){
+		tiles=setUpTiles();
+		
+	}
+	
+	/*
+	 * Sets up the initial tile distribution and returns it as an array of tiles.
+	 */
+	private Tile[] setUpTiles(){
 		try {
 			myScanner = new Scanner(new File("tileDist.txt"));
 		} catch (FileNotFoundException e) {
 			System.out.println("The filepath of the initial tiles is invalid");
 			e.printStackTrace();
 		}
-		tiles = new Tile[TILES_IN_GAME];
+		tempTiles = new Tile[TILES_IN_GAME];
 		int i = 0;
 		while (myScanner.hasNextInt()) {
 	          int north = myScanner.nextInt();
@@ -29,9 +38,14 @@ public class Carcassonne {
 	          int south = myScanner.nextInt();
 	          int west = myScanner.nextInt();
 	          int center = myScanner.nextInt();
-	          tiles[i] = new Tile(north, east, south, west, center);
+	          tempTiles[i] = new Tile(north, east, south, west, center);
 	          i++;
 	      }
+		return tempTiles;
+	}
+	
+	protected Tile[] getTiles(){
+		return tiles;
 	}
 
 }
---
+++ src/CarcassonneTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CarcassonneTest {
+
+	
+	private Tile[] tilesTest;
+	private Carcassonne carcassonne;
+	@Test
+	public void testInitialTileDistribution() {
+		carcassonne= new Carcassonne();
+		tilesTest=carcassonne.getTiles();
+		
+		
+		for(int j =0; j<tilesTest.length; j++){
+			System.out.println(tilesTest[j]);
+		}
+		
+		assertEquals(0, tilesTest[0].getNorth());
+		assertEquals(0, tilesTest[0].getEast());
+		assertEquals(0, tilesTest[0].getSouth());
+		assertEquals(0, tilesTest[0].getWest());
+		assertEquals(4, tilesTest[0].getCenter());
+		
+		assertEquals(1, tilesTest[70].getNorth());
+		assertEquals(1, tilesTest[70].getEast());
+		assertEquals(1, tilesTest[70].getSouth());
+		assertEquals(1, tilesTest[70].getWest());
+		assertEquals(3, tilesTest[70].getCenter());
+	}
+
+}
