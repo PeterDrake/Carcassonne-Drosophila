c1307b5571cd8964ac0dfeaaa8ae0bfa93b5eb72
Isabel Suhr <isabels@lclark.edu>
2014-02-04 15:30:19 -0800

    Can correctly identify completed roads that start at one endpoint

--- src/Board.java
+++ src/Board.java
@@ -23,6 +23,48 @@ public class Board {
 	
 	//this method returns true if there is at least one completed road
 	public boolean roadCompleted(){
+		Tile t = getTile(lastX, lastY);
+		boolean complete = false;
+		if(t.getNorth() == Tile.ROAD && getTile(lastX, lastY-1) != null){
+			if(searchRoad(getTile(lastX, lastY-1), t, 2, lastX, lastY-1)){
+				complete = true;
+			}
+		}else if(t.getEast() == Tile.ROAD && getTile(lastX+1, lastY) != null){
+			if(searchRoad(getTile(lastX+1, lastY), t, 3, lastX+1, lastY)){
+				complete = true;
+			}
+		}else if(t.getSouth() == Tile.ROAD && getTile(lastX, lastY+1) != null){
+			if(searchRoad(getTile(lastX, lastY+1), t, 0, lastX, lastY+1)){
+				complete = true;
+			}
+		}else if(t.getWest() == Tile.ROAD && getTile(lastX-1, lastY) != null){
+			if(searchRoad(getTile(lastX-1, lastY), t, 1, lastX-1, lastY)){
+				complete = true;
+			}
+		}
+		return complete;
+	}
+	
+	//recursive helper method for searching for road completion
+	//from is which direction this tile was entered from - north 0, east 1, south 2, west 3
+	protected boolean searchRoad(Tile current, Tile original, int from, int x, int y){
+		if((current.getCenter() == Tile.CASTLE)||(current.getCenter() == Tile.CLOISTER)
+				||(current.getCenter() == Tile.XROAD)){
+			return true;
+		}else if(current == original){
+			return true;
+		}
+		if(current.getNorth() == Tile.ROAD && from != 0 && getTile(x, y-1) != null){
+			return searchRoad(getTile(x, y-1), original, 2, x, y-1);
+		}else if(current.getEast() == Tile.ROAD && from != 1 && getTile(x+1, y) != null){
+			return searchRoad(getTile(x+1, y), original, 3, x+1, y);
+		}else if(current.getSouth() == Tile.ROAD && from != 2 && getTile(x, y+1) != null){
+			return searchRoad(getTile(x, y+1), original, 0, x, y+1);
+		}else if(current.getWest() == Tile.ROAD && from != 3 && getTile(x-1, y) != null){
+			return searchRoad(getTile(x-1, y), original, 1, x-1, y);
+		}
 		return false;
 	}
+	
+	
 }
